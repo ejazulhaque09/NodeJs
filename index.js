8fdc7284c9ccbe240296e1629cd968701fafa8b6
ejazulhaque0 <ejazulhaque09@gmail.com>
2024-03-28 23:54:06 +0530

    REST API using Express Js

--- index.js
+++ index.js
@@ -1,65 +1,70 @@
-const fs = require('fs');
-const index = fs.readFileSync('index.html', 'utf-8');
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const fs = require("fs");
+const index = fs.readFileSync("index.html", "utf-8");
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 const products = data.products;
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const server = express();
 
 //bodyParser
 server.use(express.json());
 // server.use(express.urlencoded());
-server.use(morgan('default'))
-server.use(express.static('public'));
-// server.use((req, res, next) => {
-//   console.log(
-//     req.method,
-//     req.ip,
-//     req.hostname,
-//     new Date(),
-//     req.get('User-Agent')
-//   );
-//   next();
-// });
-
-const auth = (req, res, next) => {
-  console.log(req.query);
-
-  if (req.body.password == '123') {
-    next();
-  } else {
-    res.sendStatus(401);
-  }
-  next()
- 
-};
+server.use(morgan("default"));
+server.use(express.static("public"));
 
 // API - Endpoint - Route
-server.get('/product/:id', auth, (req, res) => {
-  console.log(req.params)
-  res.json({ type: 'GET' });
+
+// Products
+//API root, base URL
+// C R U D
+// Creating API
+// Create POST /products
+server.post("/products", (req, res) => {
+  console.log(req.body);
+  products.push(req.body);
+  res.status(201).json(req.body);
 });
-server.post('/', auth, (req, res) => {
-  res.json({ type: 'POST' });
+// Read Get API
+server.get("/products", (req, res) => {
+  res.status(200).json(products);
 });
-server.put('/', (req, res) => {
-  res.json({ type: 'PUT' });
-});
-server.delete('/', (req, res) => {
-  res.json({ type: 'DELETE' });
-});
-server.patch('/', (req, res) => {
-  res.json({ type: 'PATCH' });
+// for single prroduct
+server.get("/products/:id", (req, res) => {
+  console.log(req.params);
+  const id = +req.params.id;
+  const product = products.find((p) => p.id == id); //finding id in the products matching with the id in params
+  res.status(200).json(product);
 });
 
-server.get('/demo', (req, res) => {
-  // res.sendStatus(404);
-  // res.json(products)
-  // res.status(201).send('<h1>hello</h1>')
-  // res.sendFile('/Users/abhishekrathore/Desktop/node-app/index.html')
+// UPDATE put /products/:id
+server.put("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id == id); //finding id in the products matching with the id in params
+  products.splice(productIndex,1,{id:id,...req.body})
+  res.status(202).json();
+});
+
+
+// UPDATE PATCH /products/:id
+server.patch("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id == id); //finding id in the products matching with the id in params
+  const product = products[productIndex];
+  products.splice(productIndex,1,{...product, ...req.body})
+  res.status(202).json();
+});
+
+
+// delete DELETE /products/:id
+server.delete("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id == id); //finding id in the products matching with the id in params
+  const product = products[productIndex];
+  products.splice(productIndex,1)
+  res.status(204).json(product);
 });
 
 server.listen(8080, () => {
-  console.log('server started');
+  console.log("server started");
 });
